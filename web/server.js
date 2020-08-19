3117a5639bb1d52a0b62d0ec2c34b0bbb02b499d
nalahakone@deakin.edu.au <nalahakone@deakin.edu.au>
2020-08-19 14:26:57 +1000

    ENHANCE: Updated listening port

--- web/server.js
+++ web/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 const base = `${__dirname}/public`
 
 app.use(express.static('public'))
@@ -9,25 +10,25 @@ app.use(express.static('public'))
 app.get('/', function (req, res) {
     res.sendFile(`${base}/device-list.html`);
    });
-app.get('*', (req, res) => {
-    res.sendFile(`${base}/404.html`);
+app.get('/login', (req, res) => { 
+    res.sendFile(`${base}/login.html`); 
    });
+  
 app.get('/register-device', (req, res) => {
     res.sendFile(`${base}/register-device.html`);
    });
 app.get('/send-command', (req, res) => {
     res.sendFile(`${base}/send-command.html`);
    });
--app.get('/about', (req, res) => {
+app.get('/about', (req, res) => {
     res.sendFile(`${base}/about-me.html`);
    });
-   app.get('/registration', (req, res) => { 
+app.get('/registration', (req, res) => { 
     res.sendFile(`${base}/registration.html`); 
    });
-   app.get('/login', (req, res) => { 
-    res.sendFile(`${base}/login.html`); 
+app.get('*', (req, res) => {
+    res.sendFile(`${base}/404.html`);
    });
-  
 // server listen
 app.listen(port, () => {
  console.log(`listening on port ${port}`);
